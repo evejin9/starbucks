f223cdb9d2e9412b3e9bfe5813a33c4cadd68bc2
evejin9 <evejin9@naver.com>
2023-04-13 18:32:30 +0900

    23.04.13) 스타벅스 클론 코딩
    - 가로 너비를 가진 블록 요소에 margin: 0 auto; 를 주면 부모 범위 안에서 가운데 정렬함.
    
    - 웹 사이트를 반응형으로 작업하고 싶다면?
      => width값이 아닌 max-width 값을 주면 스크롤이 생기지 않고 함께 줄어듦
    
    - 링크 주소 대신 # 을 쓰면 CSS 아이디 선택자를 이용해 페이지 내 특정 위치로 이동
      => 찾는 id 값이 없으면 페이지 최상단으로 이동하니 이게 싫으면 JS인 'javascript:void(0)' 이용
    
    - 인라인 요소를 위-아래 여백을 주기 위해서는 display에서 block으로 변경 먼저 하기.
    
    - 여백을 패딩으로 주게 되면 클릭할 수 있는 영역을 넓혀주는 효과를 보임.
    
    - 가상 요소 선택자 사용 시 content가 꼭 있어야 함. (내용이 없더라도 반드시 필요함)
    
    - absolute 상태인 요소에 top: 0, bottom:0, margin에 auto; 를 주는 이유
      => 수직 가운데 배치를 위해
    
    - box-sizing을 border-box로 변경 => margin+content+padding 포함한 크기
    
    - setAttribute => HTML의 속성을 추가하는 메소드
    
    - JS에 클래스 값을 주는 이유는 CSS에서 활용하기 위해
    
    - CSS 투명 조절: opacity
    
    - z-index: 드롭 다운 될 요소가 더 위에 보이게 하기 위해 설정
    
    - border-radius: 좌상단부터 시계 방향으로 적용됨
    
    - 이미지(인라인 요소)에 발생하는 문자 기준선(baseline) 아래 공간은 이미지를 블록 요소로 바꾸어주면 없어짐.
    
    - position: fixed; => 가로 너비를 지정하지 않으면 최소한으로 줄어드려고 함.
    
    - 자바스크립트에 입력값 넣을 때는 문자 데이터("", '', ``) 사용

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// 검색창 요소(.search) 선택 시 강제 포커스 및 제어
+const searchEl = document.querySelector('.search');
+// const searchInputEl = document.querySelector('.search input') // 아래와 같이 최적화
+const searchInputEl = searchEl.querySelector('input');
+
+// 검색창 요소를 클릭하면 input 요소를 포커스하도록 실행
+searchEl.addEventListener('click', function () { // 이벤트 핸들러
+  searchInputEl.focus(); // 포커스 강제 적용
+})
+
+// input 요소에 포커스 되면 실핼
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색'); // HTML 속성을 추가하는 메소드
+})
+
+// input 요소에 포커스가 해제(블러 )되면 실핼
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', ''); // HTML 속성을 추가하는 메소드
+})
+
+// 스크롤 시 전역 배치(고정 배너) 숨기기
+const badgeEl = document.querySelector('header .badges');
+
+// window: 브라우저 창 객체
+window.addEventListener('scroll', function () {
+  // console.log(window.scrollY); // y축으로 얼마나 스크롤 했는지에 대한 수치
+        
+  // 만약 y축으로 스크롤 한 수치가 500을 초과하면 배지 요소를 숨기고
+  // 그렇지 않으면 다시 보이기!!
+
+  if (window.scrollY > 500) {
+    // 배지 요소 숨기기
+    badgeEl.style.display = "none";
+  } else {
+    // 배지 요소 보이기!
+    badgeEl.style.display = "block";
+  }
+});
+
