64078a1461816a9ebadc0f52a85695995317754f
evejin9 <evejin9@naver.com>
2023-04-14 18:58:40 +0900

    23.04.14) 스타벅스 클론 코딩
    <애니메이션 라이브러리 적용>
    - 순서 주의 (main.js에서 사용하니 js 연결 링크보다 더 앞에 위치)
    
    <애니메이션 처리 시 주의사항>
    - display 속성은 animation 효과가 먹히지 않음.
    
    <헤더가 fixed 되어 일반 배치 흐름에서 벗어나 다음 요소가 밑으로 들어간 경우>
    - 헤더의 높이만큼 외부 여백 추가
    
    <gsap.to 메소드>
    - 움직임의 끝나는 점을 지정하는 애니메이션
    - 괄호 안에 (요소, 지속시간, 옵션 {}) 을 넣어야 함.
    
    <수직 슬라이드를 스와이프 하고 싶을 때>
    - 높이를 꼭 주어야 적용됨.

--- js/main.js
+++ js/main.js
@@ -32,10 +32,43 @@ window.addEventListener('scroll', function () {
 
   if (window.scrollY > 500) {
     // 배지 요소 숨기기
-    badgeEl.style.display = "none";
+    // badgeEl.style.display = "none";
+
+    // gsap.to(요소, 지속시간, ,옵션 {}) 메소드: CSS 속성을 통해 애니메이션 처리
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0, 
+      display: 'none'
+    });
   } else {
     // 배지 요소 보이기!
-    badgeEl.style.display = "block";
+    // badgeEl.style.display = "block";
+
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1, 
+      display: 'block'
+    });
   }
 });
 
+// 순차적으로 VISUAL 섹션 내 요소 보이기
+// 나타날 요소(.fade-in)들을 찾기
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+// 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEls, index) {
+  // gsap.to(요소, 지속시간, ,옵션 {})
+  gsap.to(fadeEls, 1, {
+    // delay: 몇 초 뒤에 실행될 것인가?
+    delay: (index + 1) * 0.7, // 0.7, 1.4, 2.1, 2.8
+    opacity: 1
+  });
+});
+
+// 공지사항 수직 슬라이드 기능 작성
+// new 키워드로 Swiper 객체를 생성 => 슬라이드 기능을 생성
+// new Swiper(선택자, 옵션: {});
+new Swiper('.notice .swiper', {
+  direction: 'vertical', // 수직 슬라이드
+  loop: true, // 반복 재생 여부
+  autoplay: true // 자동 재생 여부
+});
